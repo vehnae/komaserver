70d0d59eab3eb0abbc14cfda8e6a511ff338f15e
Jari Saukkonen <jari.saukkonen@taika.fi>
2016-03-06 11:51:23 +0200

    Add weather update script and implement /weather json api

--- server.js
+++ server.js
@@ -11,16 +11,23 @@ app.all('*', function(req, res, next) {
 });
 
 app.get('/weather', function(req, res) {
-    // luetaan sääasema ja jutut
-    var data = {
-      temperature: 5.3,
-      humidity: 90,
-      pressure: 1001,
-      rain: 0,
-      clouds: 0
-    };
+    req.memcache.get('openweathermap', function(error, result) {
+        if (error) {
+            console.log('error reading memcached: ' + error + ' result: ' + result);
+            return res.status(500).end();
+        }
 
-    res.json(data);
+        var weatherdata = JSON.parse(result);
+        var data = {
+            temperature: weatherdata.main.temp,
+            humidity: weatherdata.main.humidity,
+            pressure: weatherdata.main.pressure,
+            windspeed: weatherdata.wind.speed,
+            winddir: weatherdata.wind.deg,
+            clouds: weatherdata.clouds.all
+        };
+        res.json(data);
+    });
 });
 
 app.get('/safe', function(req, res) {
@@ -28,7 +35,9 @@ app.get('/safe', function(req, res) {
     // luetaan rajakytkimet, sääasemat sun muut härpättimet josta tila selviää
     var safe = true;
 
-    res.json({ safe: safe });
+    res.json({
+        safe: safe
+    });
 });
 
 app.param('user', function(req, res, next, user) {
@@ -38,7 +47,7 @@ app.param('user', function(req, res, next, user) {
 
 app.all('/roof/*', function(req, res, next) {
     req.memcache.get('roof-state', function(error, result) {
-        req.roofstate = JSON.parse(result) || {};
+        req.roofstate = JSON.parse(result) ||  {};
         var state = _.clone(req.roofstate);
         console.log('roof state before: ' + JSON.stringify(req.roofstate));
         next();
@@ -51,12 +60,15 @@ app.all('/roof/*', function(req, res, next) {
 });
 
 app.get('/roof/:user', function(req, res) {
-    res.json({ open: req.roofstate[req.user] });
+    res.json({
+        open: req.roofstate[req.user]
+    });
 });
 
 app.post('/roof/:user/open', function(req, res) {
-    if (_.every(_.values(req.roofstate), function(v, index) { return v == false; }))
-    {
+    if (_.every(_.values(req.roofstate), function(v, index) { 
+            return v == false;
+        })) {
         console.log('open physical roof');
     }
 
@@ -65,10 +77,13 @@ app.post('/roof/:user/open', function(req, res) {
 });
 
 app.post('/roof/:user/close', function(req, res) {
-    var roofopen = _.any(_.values(req.roofstate), function(v, index) { return v == true; });
+    var roofopen = _.any(_.values(req.roofstate), function(v, index) { 
+        return v == true;
+    });
     req.roofstate[req.user] = false;
-    if (roofopen && _.every(_.values(req.roofstate), function(v, index) { return v == false; }))
-    {
+    if (roofopen && _.every(_.values(req.roofstate), function(v, index) { 
+            return v == false;
+        })) {
         console.log('close physical roof');
     }
     res.status(200).send('OK');
