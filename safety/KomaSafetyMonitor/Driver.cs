7c88ddcad05082ff5a5724b154f85b860a0c6fa3
Jari Saukkonen <jari.saukkonen@ambientia.fi>
2016-07-31 10:43:30 +0300

    Initial implementation for safety monitor

---
+++ safety/KomaSafetyMonitor/Driver.cs
@@ -0,0 +1,418 @@
+//tabs=4
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Runtime.InteropServices;
+
+using ASCOM;
+using ASCOM.Astrometry;
+using ASCOM.Astrometry.AstroUtils;
+using ASCOM.Utilities;
+using ASCOM.DeviceInterface;
+using System.Globalization;
+using System.Collections;
+
+namespace ASCOM.Komakallio
+{
+    /// <summary>
+    /// ASCOM SafetyMonitor Driver for KomaObservatory.
+    /// </summary>
+    [Guid("be987b5a-31ab-4f18-8dda-861a5a307d5d")]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class SafetyMonitor : ISafetyMonitor
+    {
+        /// <summary>
+        /// ASCOM DeviceID (COM ProgID) for this driver.
+        /// The DeviceID is used by ASCOM applications to load the driver at runtime.
+        /// </summary>
+        internal static string driverID = "ASCOM.Komakallio.SafetyMonitor";
+        // TODO Change the descriptive string for your driver then remove this line
+        /// <summary>
+        /// Driver description that displays in the ASCOM Chooser.
+        /// </summary>
+        private static string driverDescription = "Komakallio SafetyMonitor Driver";
+
+        internal static string serverAddressProfileName = "Server Address"; // Constants used for Profile persistence
+        internal static string serverAddressDefault = "http://192.168.0.110:9000/safety";
+        internal static string serverAddress;
+
+        // Data
+        private boolean safe = false;
+        private DateTime lastUpdate;
+        private TimerCallback updateTimerDelegate;
+        private System.Threading.Timer updateTimer;
+
+        /// <summary>
+        /// Private variable to hold the connected state
+        /// </summary>
+        private bool connectedState;
+
+        /// <summary>
+        /// Private variable to hold an ASCOM Utilities object
+        /// </summary>
+        private Util utilities;
+
+        /// <summary>
+        /// Private variable to hold an ASCOM AstroUtilities object to provide the Range method
+        /// </summary>
+        private AstroUtils astroUtilities;
+
+        /// <summary>
+        /// Private variable to hold the trace logger object (creates a diagnostic log file with information that you specify)
+        /// </summary>
+        private TraceLogger tl;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KomaObservatory"/> class.
+        /// Must be public for COM registration.
+        /// </summary>
+        public SafetyMonitor()
+        {
+            ReadProfile(); // Read device configuration from the ASCOM Profile store
+
+            tl = new TraceLogger("", "KomaObservatory");
+            tl.LogMessage("SafetyMonitor", "Starting initialisation");
+
+            connectedState = false; // Initialise connected to false
+            utilities = new Util(); //Initialise util object
+            astroUtilities = new AstroUtils(); // Initialise astro utilities object
+
+            updateTimerDelegate = new TimerCallback(UpdateObservingConditionsData);
+
+            tl.LogMessage("SafetyMonitor", "Completed initialisation");
+        }
+
+
+        //
+        // PUBLIC COM INTERFACE ISafetyMonitor IMPLEMENTATION
+        //
+
+        #region Common properties and methods.
+
+        /// <summary>
+        /// Displays the Setup Dialog form.
+        /// If the user clicks the OK button to dismiss the form, then
+        /// the new settings are saved, otherwise the old values are reloaded.
+        /// THIS IS THE ONLY PLACE WHERE SHOWING USER INTERFACE IS ALLOWED!
+        /// </summary>
+        public void SetupDialog()
+        {
+            // consider only showing the setup dialog if not connected
+            // or call a different dialog if connected
+            if (IsConnected)
+                System.Windows.Forms.MessageBox.Show("Already connected, just press OK");
+
+            using (SetupDialogForm F = new SetupDialogForm())
+            {
+                var result = F.ShowDialog();
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    WriteProfile(); // Persist device configuration values to the ASCOM Profile store
+                }
+            }
+        }
+
+        public ArrayList SupportedActions
+        {
+            get
+            {
+                tl.LogMessage("SupportedActions Get", "Returning empty arraylist");
+                return new ArrayList();
+            }
+        }
+
+        public string Action(string actionName, string actionParameters)
+        {
+            throw new ASCOM.ActionNotImplementedException("Action " + actionName + " is not implemented by this driver");
+        }
+
+        public void CommandBlind(string command, bool raw)
+        {
+            CheckConnected("CommandBlind");
+            throw new ASCOM.MethodNotImplementedException("CommandBlind");
+        }
+
+        public bool CommandBool(string command, bool raw)
+        {
+            CheckConnected("CommandBool");
+            throw new ASCOM.MethodNotImplementedException("CommandBlind");
+        }
+
+        public string CommandString(string command, bool raw)
+        {
+            CheckConnected("CommandString");
+            throw new ASCOM.MethodNotImplementedException("CommandBlind");
+        }
+
+        public void Dispose()
+        {
+            if (this.Connected)
+                this.Connected = false;
+            // Clean up the tracelogger and util objects
+            tl.Enabled = false;
+            tl.Dispose();
+            tl = null;
+            utilities.Dispose();
+            utilities = null;
+            astroUtilities.Dispose();
+            astroUtilities = null;
+        }
+
+        public bool Connected
+        {
+            get
+            {
+                tl.LogMessage("Connected Get", IsConnected.ToString());
+                return IsConnected;
+            }
+            set
+            {
+                tl.LogMessage("Connected Set", value.ToString());
+                if (value == IsConnected)
+                    return;
+
+                if (value)
+                {
+                    connectedState = true;
+                    LogMessage("Connected Set", "Connecting to server {0}", serverAddress);
+
+                    if (updateTimer == null)
+                    {
+                        updateTimer = new System.Threading.Timer(updateTimerDelegate, null, 0, (10 * 1000));
+                    }
+                }
+                else
+                {
+                    connectedState = false;
+                    LogMessage("Connected Set", "Disconnecting from server {0}", serverAddress);
+
+                    if (updateTimer != null)
+                    {
+                        updateTimer.Dispose();
+                        updateTimer = null;
+                    }
+                }
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                tl.LogMessage("Description Get", driverDescription);
+                return driverDescription;
+            }
+        }
+
+        public string DriverInfo
+        {
+            get
+            {
+                Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+                string driverInfo = "Information about the driver itself. Version: " + String.Format(CultureInfo.InvariantCulture, "{0}.{1}", version.Major, version.Minor);
+                tl.LogMessage("DriverInfo Get", driverInfo);
+                return driverInfo;
+            }
+        }
+
+        public string DriverVersion
+        {
+            get
+            {
+                Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+                string driverVersion = String.Format(CultureInfo.InvariantCulture, "{0}.{1}", version.Major, version.Minor);
+                tl.LogMessage("DriverVersion Get", driverVersion);
+                return driverVersion;
+            }
+        }
+
+        public short InterfaceVersion
+        {
+            // set by the driver wizard
+            get
+            {
+                tl.LogMessage("InterfaceVersion Get", "1");
+                return Convert.ToInt16("1");
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                string name = "KomaSafetyMonitor";
+                tl.LogMessage("Name Get", name);
+                return name;
+            }
+        }
+
+        #endregion
+
+        #region ISafetyMonitor Implementation
+        public bool IsSafe
+        {
+            get
+            {
+                return safe;
+            }
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void UpdateSafetyMonitorData(object state)
+        {
+            var request = WebRequest.Create(serverAddress) as HttpWebRequest;
+            try
+            {
+                using (var response = request.GetResponse() as HttpWebResponse)
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                    {
+                        throw new Exception(String.Format("Server error (HTTP {0}: {1}).",
+                            response.StatusCode,
+                            response.StatusDescription));
+                    }
+
+                    string json;
+                    using (var responseStream = response.GetResponseStream())
+                    using (var reader = new StreamReader(responseStream, System.Text.Encoding.UTF8))
+                        json = reader.ReadToEnd();
+
+                    Dictionary<string, string> values = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+                    safe = Boolean.Parse(values["safe"], CultureInfo.InvariantCulture);
+                    lastUpdate = DateTime.Now;
+                }
+            } catch( Exception e)
+            {
+                LogMessage("Update", "Error: " + e.Message);
+            }
+        }
+
+        #region ASCOM Registration
+
+        // Register or unregister driver for ASCOM. This is harmless if already
+        // registered or unregistered.
+        //
+        /// <summary>
+        /// Register or unregister the driver with the ASCOM Platform.
+        /// This is harmless if the driver is already registered/unregistered.
+        /// </summary>
+        /// <param name="bRegister">If <c>true</c>, registers the driver, otherwise unregisters it.</param>
+        private static void RegUnregASCOM(bool bRegister)
+        {
+            using (var P = new ASCOM.Utilities.Profile())
+            {
+                P.DeviceType = "SafetyMonitor";
+                if (bRegister)
+                {
+                    P.Register(driverID, driverDescription);
+                }
+                else
+                {
+                    P.Unregister(driverID);
+                }
+            }
+        }
+
+        /// <summary>
+        /// This function registers the driver with the ASCOM Chooser and
+        /// is called automatically whenever this class is registered for COM Interop.
+        /// </summary>
+        /// <param name="t">Type of the class being registered, not used.</param>
+        /// <remarks>
+        /// This method typically runs in two distinct situations:
+        /// <list type="numbered">
+        /// <item>
+        /// In Visual Studio, when the project is successfully built.
+        /// For this to work correctly, the option <c>Register for COM Interop</c>
+        /// must be enabled in the project settings.
+        /// </item>
+        /// <item>During setup, when the installer registers the assembly for COM Interop.</item>
+        /// </list>
+        /// This technique should mean that it is never necessary to manually register a driver with ASCOM.
+        /// </remarks>
+        [ComRegisterFunction]
+        public static void RegisterASCOM(Type t)
+        {
+            RegUnregASCOM(true);
+        }
+
+        /// <summary>
+        /// This function unregisters the driver from the ASCOM Chooser and
+        /// is called automatically whenever this class is unregistered from COM Interop.
+        /// </summary>
+        /// <param name="t">Type of the class being registered, not used.</param>
+        /// <remarks>
+        /// This method typically runs in two distinct situations:
+        /// <list type="numbered">
+        /// <item>
+        /// In Visual Studio, when the project is cleaned or prior to rebuilding.
+        /// For this to work correctly, the option <c>Register for COM Interop</c>
+        /// must be enabled in the project settings.
+        /// </item>
+        /// <item>During uninstall, when the installer unregisters the assembly from COM Interop.</item>
+        /// </list>
+        /// This technique should mean that it is never necessary to manually unregister a driver from ASCOM.
+        /// </remarks>
+        [ComUnregisterFunction]
+        public static void UnregisterASCOM(Type t)
+        {
+            RegUnregASCOM(false);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Returns true if there is a valid connection to the driver hardware
+        /// </summary>
+        private bool IsConnected
+        {
+            get
+            {
+                // TODO check that the driver hardware connection exists and is connected to the hardware
+                return connectedState;
+            }
+        }
+
+        /// <summary>
+        /// Use this function to throw an exception if we aren't connected to the hardware
+        /// </summary>
+        /// <param name="message"></param>
+        private void CheckConnected(string message)
+        {
+            if (!IsConnected)
+            {
+                throw new ASCOM.NotConnectedException(message);
+            }
+        }
+
+        /// <summary>
+        /// Read the device configuration from the ASCOM Profile store
+        /// </summary>
+        internal void ReadProfile()
+        {
+            using (Profile driverProfile = new Profile())
+            {
+                driverProfile.DeviceType = "SafetyMonitor";
+                serverAddress = driverProfile.GetValue(driverID, serverAddressProfileName, string.Empty, serverAddressDefault);
+            }
+        }
+
+        /// <summary>
+        /// Write the device configuration to the  ASCOM  Profile store
+        /// </summary>
+        internal void WriteProfile()
+        {
+            using (Profile driverProfile = new Profile())
+            {
+                driverProfile.DeviceType = "SafetyMonitor";
+                driverProfile.WriteValue(driverID, serverAddressProfileName, serverAddress.ToString());
+            }
+        }
+
+        #endregion
+    }
+}
