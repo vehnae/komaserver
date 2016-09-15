0c584c779ee376d02ab621763a578b49c6bb94eb
Jari Saukkonen <jari.saukkonen@taika.fi>
2016-09-15 23:16:26 +0300

    simple motor interface

---
+++ motor/server.js
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2016 Jari Saukkonen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+var COMPORT = '/dev/cu.wchusbserial14420';
+
+var express = require('express');
+var _ = require('underscore');
+var expressLogging = require('express-logging');
+var logger = require('logops');
+logger.setLevel('DEBUG');
+var SerialPort = require('serialport');
+var app = express();
+var nmea0183 = require('./nmea0183');
+var parser = nmea0183();
+
+var power = [];
+for (var i = 0; i < 120; i++) {
+    power.push(0);
+}
+var status = "";
+
+var port = new SerialPort(COMPORT, {
+  baudRate: 57600
+});
+
+
+app.get('/status', function(req, res) {
+    res.json({
+        power: power,
+        status: status
+    });
+});
+
+app.post('/open', function(req, res) {
+    port.write('$OPEN*14\r\n');
+    res.json({success:true});
+});
+
+app.post('/close', function(req, res) {
+    port.write('$CLOSE*56\r\n');
+    res.json({success:true});
+});
+
+app.post('/stop', function(req, res) {
+    port.write('$STOP*18\r\n');
+    res.json({success:true});
+});
+
+app.use(express.static('public'));
+
+port.on('open', function() {
+    logger.info('serial port opened');
+});
+
+port.on('data', function (data) {
+    var handlers = {
+        POWER:function(args) {
+            var data = args.split(',');
+            for (var i = 0; i < data.length; i++) {
+                power.shift();
+                power.push(parseInt(data[i]));
+            }
+        },
+        STATUS:function(args) {
+            status = args;
+        }
+    };
+
+    data = data.toString();
+    for (var i = 0; i < data.length; i++) {
+        var result = parser.consume(data.charAt(i));
+        if (result.complete && result.success) {
+            var cmd = result.message.substring(0, result.message.indexOf(','));
+            if (handlers[cmd])
+                handlers[cmd](result.message.substring(result.message.indexOf(',')+1));
+        }
+    }
+});
+
+var server = app.listen(9000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    logger.info('listening at http://%s:%s', host, port);
+}).on('error', function(err) {
+    logger.error('on error handler');
+    logger.error(err);
+});
