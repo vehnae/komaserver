39471d62b073a411a37434bc01756c0e67012681
Jari Saukkonen <jari.saukkonen@ambientia.fi>
2017-06-03 21:31:28 +0300

    add license

--- server/server.js
+++ server/server.js
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2017 Jari Saukkonen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 var express = require('express');
 var _ = require('underscore');
 var memcache = require('memcache');
@@ -15,43 +38,6 @@ app.all('*', function(req, res, next) {
     client.connect();
     req.memcache = client;
     next();
-});
-
-app.get('/weather', function(req, res) {
-    req.memcache.get('openweathermap', function(error, result) {
-        if (error) {
-            logger.error('error reading memcached: ' + error + ' result: ' + result);
-            return res.status(500).end();
-        }
-
-        var weatherdata = JSON.parse(result);
-        var data = {
-            temperature: weatherdata.main.temp,
-            humidity: weatherdata.main.humidity,
-            pressure: weatherdata.main.pressure,
-            windspeed: weatherdata.wind.speed,
-            winddir: weatherdata.wind.deg,
-            clouds: weatherdata.clouds.all
-        };
-        res.json(data);
-    });
-});
-
-function updateSafety() {
-    // luetaan rajakytkimet, sääasemat sun muut härpättimet josta tila selviää
-    fs.access('/tmp/.unsafe', fs.F_OK, function(err) {
-        if (!err) 
-            safe = false;
-        else
-            safe = true;
-    });
-    setTimeout(updateSafety, 5000);
-}
-
-app.get('/safety', function(req, res) {
-    res.json({
-        safe: safe
-    });
 });
 
 app.param('user', function(req, res, next, user) {
