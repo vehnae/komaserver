71c709ea3a715c3c89af2186ecf889c4b4753c22
Jari Saukkonen <jari.saukkonen@ambientia.fi>
2017-06-16 08:14:41 +0300

    graph y-scale to 4000mA; better default port

--- motor/server.js
+++ motor/server.js
@@ -21,7 +21,7 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-var COMPORT = '/dev/cu.wchusbserial14420';
+var COMPORT = '/dev/cu.usbmodem1411';
 
 var express = require('express');
 var _ = require('underscore');
