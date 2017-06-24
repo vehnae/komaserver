bdea0f78406d4fe918446744e15557a39882fe64
Jari Saukkonen <jari.saukkonen@taika.fi>
2017-06-25 00:57:15 +0300

    Remove memcache in favor for redis

--- roof/server.js
+++ roof/server.js
@@ -21,23 +21,22 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-var express = require('express');
-var _ = require('underscore');
-var memcache = require('memcache');
-var expressLogging = require('express-logging');
-var logger = require('logops');
+const express = require('express');
+const _ = require('underscore');
+const expressLogging = require('express-logging');
+const logger = require('logops');
 logger.setLevel('DEBUG');
-var fs = require('fs');
+const fs = require('fs');
+const redis = require('redis');
 
-var app = express();
+const app = express();
 app.use(expressLogging(logger));
-var safe = false;
 
-app.all('*', function(req, res, next) {
-    client = new memcache.Client(11211, 'localhost');
-    client.connect();
-    req.memcache = client;
-    next();
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const redisClient = redis.createClient(REDIS_PORT);
+
+redisClient.on("error", function(err) {
+    console.log("Redis error: " + err);
 });
 
 app.param('user', function(req, res, next, user) {
@@ -46,9 +45,9 @@ app.param('user', function(req, res, next, user) {
 });
 
 app.all('/roof/*', function(req, res, next) {
-    req.memcache.get('roof-state', function(error, result) {
+     redisClient.get('roof-state', function(error, result) {
         if (error) {
-            logger.error('error reading memcached: ' + error + ' result: ' + result);
+            logger.error('error reading redis: ' + error + ' result: ' + result);
             return res.status(500).end();
         }
         req.roofstate = JSON.parse(result) || { opening:false, closing:false, openers:[], users:{} };
@@ -57,15 +56,15 @@ app.all('/roof/*', function(req, res, next) {
         var newState = JSON.stringify(req.roofstate);
         if (!_.isEqual(newState, state)) {
             logger.debug('roof state changed: ' + newState);
-            req.memcache.set('roof-state', newState);
+            redisClient.set('roof-state', newState);
         }
     });
 });
 
 app.get('/roof/:user', function(req, res) {
     res.json({
-	    open: req.roofstate.users[req.user],
-	    opening: req.roofstate.opening,
+        open: req.roofstate.users[req.user],
+        opening: req.roofstate.opening,
         closing: req.roofstate.closing
     });
 });
@@ -78,23 +77,23 @@ app.post('/roof/:user/open', function(req, res) {
         req.roofstate.openers.push(req.user);
         if (!req.roofstate.opening) {
             logger.info('open physical roof');
-	        req.roofstate.opening = true;
-	        setTimeout(function() {
-	            req.memcache.get('roof-state', function(error, result) {
-	                roofstate = JSON.parse(result);
-		            roofstate.opening = false;
+            req.roofstate.opening = true;
+            setTimeout(function() {
+                 redisClient.get('roof-state', function(error, result) {
+                    roofstate = JSON.parse(result);
+                    roofstate.opening = false;
                     roofstate.openers.forEach(function(user) {
                         roofstate.users[user] = true;
                     });
                     roofstate.openers = [];
-		            logger.debug('roof state changed: ' + JSON.stringify(roofstate));
-		            req.memcache.set('roof-state', JSON.stringify(roofstate));
-	            });
-	        }, 5000);
+                    logger.debug('roof state changed: ' + JSON.stringify(roofstate));
+                    redisClient.set('roof-state', JSON.stringify(roofstate));
+                });
+            }, 5000);
         }
     }
     else {
-	    // no need to move roof, just mark as open unless we are closing down
+        // no need to move roof, just mark as open unless we are closing down
         if (!req.roofstate.closing)
             req.roofstate.users[req.user] = true;
     }
@@ -117,19 +116,19 @@ app.post('/roof/:user/close', function(req, res) {
     remove(req.roofstate.openers, req.user);
     if (roofopen && !otherusers && !req.roofstate.closing) {
         logger.info('close physical roof');
-	    req.roofstate.closing = true;
-	    setTimeout(function() {
-	        req.memcache.get('roof-state', function(error, result) {
-	            roofstate = JSON.parse(result);
-		        roofstate.closing = false;
+        req.roofstate.closing = true;
+        setTimeout(function() {
+            redisClient.get('roof-state', function(error, result) {
+                roofstate = JSON.parse(result);
+                roofstate.closing = false;
                 roofstate.users[req.user] = false;
-	            logger.debug('roof state changed: ' + JSON.stringify(roofstate));
-		        req.memcache.set('roof-state', JSON.stringify(roofstate));
-	        });
-	    }, 5000);
+                logger.debug('roof state changed: ' + JSON.stringify(roofstate));
+                redisClient.set('roof-state', JSON.stringify(roofstate));
+            });
+        }, 5000);
     }
     else {
-	    // no need to move roof, just mark as closed unless we are opening
+        // no need to move roof, just mark as closed unless we are opening
         if (!req.roofstate.opening)
             req.roofstate.users[req.user] = false;
     }
@@ -140,9 +139,7 @@ var server = app.listen(9000, function() {
     var host = server.address().address;
     var port = server.address().port;
 
-    updateSafety();
-
-    logger.info('komaserver listening at http://%s:%s', host, port);
+    logger.info('koma-roof-server listening at http://%s:%s', host, port);
 }).on('error', function(err) {
     logger.error('on error handler');
     logger.error(err);
